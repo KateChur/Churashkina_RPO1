04226fe5bb3de8f415cc83338ddb2da9de7b6421
Kate Churashkina <chur12kj@mail.ru>
2023-03-19 14:28:36 +0300

    LR-1

---
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -0,0 +1,53 @@
+    package ru.iu3.fclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import ru.iu3.fclient.databinding.ActivityMainBinding;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'fclient' library on application startup.
+    static {
+        System.loadLibrary("fclient");
+        System.loadLibrary("mbedcrypto");
+    }
+
+    private ActivityMainBinding binding;
+
+    //public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        int res = initRng();
+        byte[] v = randomBytes(10);
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'fclient' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
+
+
+
+
+
